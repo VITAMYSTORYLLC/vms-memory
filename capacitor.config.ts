90f7011a3e62aa8fc5af34bf4f84389116912d89
Ulises Bedia <Ulibd@COmpus-MacBook-Pro.local>
2026-01-12 21:42:29 -0600

    feat: init Capacitor with iOS and Android platforms

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vms.memory',
+  appName: 'VMS Memory',
+  webDir: 'out'
+};
+
+export default config;
