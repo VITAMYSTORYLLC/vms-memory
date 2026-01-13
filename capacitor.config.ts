a73f112784f3897ff4c690fb9b3dba6dbb6c0dc3
Ulises Bedia <Ulibd@COmpus-MacBook-Pro.local>
2026-01-12 22:12:38 -0600

    chore: remove Capacitor and revert to standard web app

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.vms.memory',
-  appName: 'VMS Memory',
-  webDir: 'out'
-};
-
-export default config;
--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // This allows the app to work on a phone without a server
-  images: {
-    unoptimized: true, // This is required for mobile apps
-  },
 }
 
 module.exports = nextConfig
